22a35e978e0106641f914d4a82e3a3e949f8f9d8
gama-gabriel <bielmgama@gmail.com>
2024-12-02 04:45:36 -0300

    cors config

--- src/main/java/com/example/auth/infra/security/CorsConfig.java
+++ src/main/java/com/example/auth/infra/security/CorsConfig.java
@@ -15,7 +15,8 @@ public class CorsConfig {
         registry.addMapping("/**")
           .allowedOrigins("http://localhost:4200")
           .allowedMethods("GET", "POST", "PUT", "DELETE")
-          .allowedHeaders("*");
+          .allowedHeaders("*")
+          .allowCredentials(true);
       }
     };
   }
